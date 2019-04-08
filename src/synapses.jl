cca9c86522cff13d8c75b264737f901188dbfc8d
Konstantinos Samaras-Tsakiris <ksamtsak@gmail.com>
2019-04-08 14:53:33 +0300

    Fix synapse indexing and increase its test coverage

--- src/synapses.jl
+++ src/synapses.jl
@@ -1,3 +1,4 @@
+# ## Synapse type definitions
 """
   Synapses is basically a wrapper around a (possibly sparse) matrix.
     Connects pre- & post-synaptic entities.
@@ -31,7 +32,6 @@ struct DenseSynapses{Npre,Npost,Nsum} <: AbstractSynapses{Npre,Npost,Nsum}
   end
 end
 
-
 function (DenseSynapses(preDims::NTuple{Npre,IntT}, postDims::NTuple{Npost,IntT}, fInit= zeros)
           where {Npre,Npost, IntT<:Integer})
   DenseSynapses{Npre,Npost,Npre+Npost}(
@@ -49,11 +49,18 @@ function (SparseSynapses(preDims::NTuple{Npre,IntT}, postDims::NTuple{Npost,IntT
   )
 end
 
+
+# ## Common methods
+
 Base.size(S::AbstractSynapses)= (S.preDims..., S.postDims...) #size(S.data)
-Base.show(S::SparseSynapses)= show(S.data)
-Base.show(io, mime::MIME"text/plain", S::SparseSynapses)= show(io,mime,S.data)
-Base.similar(S::AbstractSynapses, ::Type{elT}, idx::Vararg{Int,N}) where {elT,N}=
+Base.show(S::AbstractSynapses)= show(S.data)
+Base.show(io, mime::MIME"text/plain", S::AbstractSynapses)= show(io,mime,S.data)
+Base.display(S::AbstractSynapses)= display(S.data)
+Base.similar(S::AbstractSynapses, ::Type{elT}, idx::Dims) where {elT}=
     similar(S.data, elT, idx)
+
+
+# ## Dense Synapses
 
 Base.@propagate_inbounds \
 Base.getindex(S::DenseSynapses, I::Vararg{Int,N}) where {N}= S.data[I...]
@@ -61,13 +68,19 @@ Base.getindex(S::DenseSynapses, I::Vararg{Int,N}) where {N}= S.data[I...]
 Base.@propagate_inbounds \
 Base.setindex!(S::DenseSynapses, v, I::Vararg{Int,N}) where {N}= (S.data[I...]= v)
 
+
+# ## Sparse Synapses
+
+SparseArrays.nnz(S::SparseSynapses)= nnz(S.data)
+
 Base.@propagate_inbounds \
 function Base.getindex(S::SparseSynapses{Npre,Npost}, I...) where {Npre,Npost}
   idx= to_indices(S,I)
-  S.data[S.preLinIdx[idx[1:Npre]...], S.postLinIdx[idx[Npre+1:Npre+Npost]...]]
+  S.data[linIdx(S.preLinIdx,idx[1:Npre]), linIdx(S.postLinIdx,idx[Npre+1:Npre+Npost])]
 end
 
-
+# NOTE reshape v from {Npre,Npost}->internal rep?
+# Special setindex! for indexing with 2 tuples {pre,post}
 const VecTuple{N,T}= Union{NTuple{N,T}, Vector{NTuple{N,T}}}
 Base.@propagate_inbounds \
 function Base.setindex!(S::SparseSynapses{Npre,Npost}, v, Ipre::VecTuple{Npre,T},
@@ -78,18 +91,22 @@ function Base.setindex!(S::SparseSynapses{Npre,Npost}, v, Ipre::VecTuple{Npre,T}
   idx= to_indices(S,I_expanded)
   _setindex_array!(S,v,idx)
 end
-
 Base.@propagate_inbounds \
 function Base.setindex!(S::SparseSynapses{Npre,Npost}, v, I...) where {Npre,Npost,N}
   #length(I) == Npre+Npost || error("All dimensions must be specified when accessing SparseSynapses")
   idx= to_indices(S,I)
   _setindex_array!(S,v,idx)
 end
-Base.@propagate_inbounds \
-_setindex_array!(S::SparseSynapses{Npre,Npost}, v, idx) where {Npre,Npost}=
-    S.data[S.preLinIdx[idx[1:Npre]...], S.postLinIdx[idx[Npre+1:Npre+Npost]...]]= v
 
-SparseArrays.nnz(S::SparseSynapses)= nnz(S.data)
+Base.@propagate_inbounds \
+_setindex_array!(S::SparseSynapses{Npre,Npost}, v::Integer, idx::NTuple{N,<:Integer}) where {Npre,Npost,N}=
+    S.data[linIdx(S.preLinIdx,idx[1:Npre]), linIdx(S.postLinIdx,idx[Npre+1:Npre+Npost])]= v
+Base.@propagate_inbounds \
+function _setindex_array!(S::SparseSynapses{Npre,Npost}, v, idx) where {Npre,Npost}
+  idxPre= linIdx(S.preLinIdx, idx[1:Npre])
+  idxPost= linIdx(S.postLinIdx, idx[Npre+1:Npre+Npost])
+  S.data[idxPre, idxPost]= reshape(v, length(idxPre), length(idxPost))
+end
 
 # TODO optimize views into SparseSynapses
 #   Views into sparse matrices lack specialized methods for mostly everything. But it looks
@@ -97,8 +114,12 @@ SparseArrays.nnz(S::SparseSynapses)= nnz(S.data)
 #   See https://github.com/JuliaLang/julia/issues/21796#issuecomment-457203294
 
 
+# ## Utility
+
 Base.@propagate_inbounds \
 function Base.getindex(iter::LinearIndices{N}, i::Vararg{Vector{<:Integer},N}) where N
   idx= collect(zip(i...))
   map(i->iter[i...], idx)
 end
+linIdx(linIdxArray, idx::NTuple{N,<:Integer}) where N= linIdxArray[idx...]
+linIdx(linIdxArray, idx)= vec(linIdxArray[idx...])
--- test/synapses_test.jl
+++ test/synapses_test.jl
@@ -5,6 +5,7 @@ logger = ConsoleLogger(stdout, Logging.Debug);
 using SparseArrays
 
 module HTMTest
+using Test
 include("../src/common.jl")
 import Random.seed!
 
@@ -15,31 +16,44 @@ spDims= (2,3);
 # Create a Synapse array with these dimensions
 synapses= DenseSynapses(inputDims, spDims, rand);
 # Test access/modify patterns
-#@views begin
-#  @test synapses[2,2,1,3] == -69
-#  @test synapses[1:3,3:end,1,end] == [77 -50 -2; 70 -91 22; 53 -32 11]
-#  @test synapses[3,4,:,:] == [-105 42 -32; 81 115 -43]
-#  synapses[3,4,:,2:3].= [1 2; 3 4];
-#  @test synapses[3,4,:,:] == [-105 1 2; 81 3 4]
-#end
+@views begin
+  @test synapses[2,2,1,3] == 0xbb
+  @test synapses[1:3,3:end,1,end] == [0x4d 0xce 0xfe;0x46 0xa5 0x16;0x35 0xe0 0x0b]
+  @test synapses[3,4,:,:] == [0x97 0x2a 0xe0;0x51 0x73 0xd5]
+  synapses[3,4,:,2:3].= [1 2; 3 4]
+  @test synapses[3,4,:,:] == [0x97 0x01 0x02;0x51 0x03 0x04]
+end
 
 # Test SparseSynapses
 # TODO compare results with ground truth
 seed!(0)
 inputDims= (100,50);
 spDims= (200,100);
-synapses= SparseSynapses(inputDims, spDims, (T,m,n)-> sprand(T,m,n,2e-4));
+synapses= SparseSynapses(inputDims, spDims, (T,m,n)-> sprand(T,m,n,2e-2));
 display(nnz(synapses))
 
 @views begin
+  # Scalar setindex
   display(synapses[2,2,1,3])
+  synapses[2,2,1,3]= SynapsePermanenceQuantization(1)
+  display(synapses[2,2,1,3])
+
+  # Non-scalar getindex
   display(synapses[1:3,3:end,1,end])
   display(synapses[3:4,3:8,5,:])
+
+  # Non-scalar setindex. Can match either the extrinsic or the intrinsic dims
   display(synapses[3:4,3:6,5,1:2])
-  synapses[3:4,3:6,5,1:2]= sprand(Int8, 8,2,1.)
-  #display(synapses[3:4,3:6,5,1:2])
+  synapses[3:4,:,5,1:2]= rand(SynapsePermanenceQuantization, 2,50,1,2)
+  display(synapses[3:4,3:6,5,1:2])
+  synapses[3:4,:,5,1:2]= sprand(SynapsePermanenceQuantization, 100,2,.5)
+  display(synapses[3:4,3:6,5,1:2])
+
+  # Colon setindex
+  synapses[:,1,1,1]= rand(SynapsePermanenceQuantization, 100)
 end
 
+# Test `similar`
 display(synapses[3,3:8,5,:])
 
 end #module
