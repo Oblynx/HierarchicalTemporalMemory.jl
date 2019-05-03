d79df1d7d5e81963a6e9d43ae57804488b8672e7
Konstantinos Samaras-Tsakiris <ksamtsak@gmail.com>
2019-05-03 09:37:34 +0300

    Fix satminus zeroing synapses

--- src/utils/arithmetic_utils.jl
+++ src/utils/arithmetic_utils.jl
@@ -1,5 +1,5 @@
-# Saturated integer arithmetic
+# Saturated integer arithmetic in [1..typemax]
 ⊕(a::T,b::T) where {T<:Unsigned}= a+b>a ? a+b : typemax(T)
-⊖(a::T,b::T) where {T<:Unsigned}= a-b<a ? a-b : T(0)
+⊖(a::T,b::T) where {T<:Unsigned}= a-b<a ? a-b : one(T)
 ⊕(a::T,b::T) where {T<:Signed}= a+b>a ? a+b : typemax(T)
-⊖(a::T,b::T) where {T<:Signed}= a-b>0 ? a-b : T(0)
+⊖(a::T,b::T) where {T<:Signed}= a-b>0 ? a-b : one(T)
