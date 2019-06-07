c591df8f066dbd8f085ff3ec6c195a1eb8e385e0
Konstantinos Samaras-Tsakiris <ksamtsak@gmail.com>
2019-06-07 22:00:00 +0300

    Fix 0 p+/- pushing synapses to typemax

--- src/utils/arithmetic_utils.jl
+++ src/utils/arithmetic_utils.jl
@@ -1,7 +1,7 @@
 # Saturated integer arithmetic in [1..typemax]
-⊕(a::T,b::T) where {T<:Unsigned}= a+b>a ? a+b : typemax(T)
-⊖(a::T,b::T) where {T<:Unsigned}= a-b<a ? a-b : one(T)
-⊕(a::T,b::T) where {T<:Signed}= a+b>a ? a+b : typemax(T)
+⊕(a::T,b::T) where {T<:Unsigned}= a+b>=a ? a+b : typemax(T)
+⊖(a::T,b::T) where {T<:Unsigned}= a-b<=a ? a-b : one(T)
+⊕(a::T,b::T) where {T<:Signed}= a+b>=a ? a+b : typemax(T)
 ⊖(a::T,b::T) where {T<:Signed}= a-b>0 ? a-b : one(T)
 
 # Side of hypercube with range φ
