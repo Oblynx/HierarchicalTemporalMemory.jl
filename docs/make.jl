7cc78d90d3ae41da62fc7ac6995a200567b69f49
Konstantinos Samaras-Tsakiris <ksamtsak@gmail.com>
2020-06-07 19:45:52 +0200

    Add CI and docs with PkgTemplates

---
+++ docs/make.jl
@@ -0,0 +1,21 @@
+using HierarchicalTemporalMemory
+using Documenter
+
+makedocs(;
+    modules=[HierarchicalTemporalMemory],
+    authors="Konstantinos Samaras-Tsakiris <ksamtsak@gmail.com> and contributors",
+    repo="https://github.com/oblynx/HierarchicalTemporalMemory.jl/blob/{commit}{path}#L{line}",
+    sitename="HierarchicalTemporalMemory.jl",
+    format=Documenter.HTML(;
+        prettyurls=get(ENV, "CI", "false") == "true",
+        canonical="https://oblynx.github.io/HierarchicalTemporalMemory.jl",
+        assets=String[],
+    ),
+    pages=[
+        "Home" => "index.md",
+    ],
+)
+
+deploydocs(;
+    repo="github.com/oblynx/HierarchicalTemporalMemory.jl",
+)
