a9636fb0a856e56cb6a98bdf532e170a54856ad5
Konstantinos Samaras-Tsakiris <ksamtsak@gmail.com>
2020-06-09 14:16:02 +0200

    Test group enums

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,16 +1,22 @@
 ENV["JULIA_DEBUG"] = "HierarchicalTemporalMemory"
 using HierarchicalTemporalMemory, Test
 
-const GROUP = get(ENV, "GROUP", "All")
+@enum GROUPS All SpatialPooler TemporalMemory
+
+const GROUP= try
+  get(ENV, "GROUP", "All")|> Symbol|> eval
+catch e
+  error("ENV[\"GROUP\"]: Invalid value. Supported values: $(GROUPS|>instances)")
+end
 global plot_enabled= false
 
 @testset "Topology" begin include("topology_test.jl") end
-if GROUP == "ALL" || GROUP == "SpatialPooler"
+if GROUP == All || GROUP == SpatialPooler
   @testset "Spatial Pooler" begin
     include("spatial_pooler_test.jl")
   end
 end
-if GROUP == "ALL" || GROUP == "TemporalMemory"
+if GROUP == All || GROUP == TemporalMemory
   @testset "Temporal Memory" begin
     include("temporal_memory_test.jl")
   end
