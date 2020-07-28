4802c3309c1bde1186610069807fee9db0c8eede
Konstantinos Samaras-Tsakiris <ksamtsak@gmail.com>
2020-07-28 17:03:07 +0200

    Fix benchmarkCI

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -11,12 +11,14 @@ SUITE["TM"] = BenchmarkGroup()
 sp, refTM, data, encode, decoder= setupHTMexperiment()
 z,_= encode(data,1)
 
+seed!(0)
 let grp= SUITE["SP"]
   # Run 1 iteration of SP adaptation repeatedly
-  grp["SP: 1 iteration, global inhibit"]= @benchmarkable step!(sp,z)
-  grp["SP: 1 iteration, local inhibit"]= @benchmarkable step!($(@set sp.params.enable_local_inhibit= true) ,z)
+  grp["1 iteration, global inhibit"]= @benchmarkable step!(sp,z)
+  grp["1 iteration, local inhibit"]= @benchmarkable step!($(@set sp.params.enable_local_inhibit= true) ,z)
 end
 
+seed!(0)
 let grp= SUITE["TM"]
   tN= size(data,1)
   a= Vector{sp(z)|>typeof}(undef,tN)
@@ -26,17 +28,16 @@ let grp= SUITE["TM"]
   end
   # The TM grows new dendrites/synapses at every step, and it will try to grow more
   # if the input is random. So try giving it reasonable input sequences.
-  grp["TM: first 100 steps"]= @benchmarkable for t= 1:100
+  grp["first 100 steps"]= @benchmarkable for t= 1:100
       step!(tm,$a[t])
     end setup=( tm= deepcopy($refTM) )
 
   for t= 1 : tN-100
     step!(refTM,a[t])
   end
-  grp["TM: last 100 steps"]= @benchmarkable for t= $tN-99:$tN
+  grp["last 100 steps"]= @benchmarkable for t= $tN-99:$tN
       step!(tm,$a[t])
     end setup=( tm= deepcopy($refTM) )
 end
 
-#tune!(SUITE)
 #results= run(SUITE, verbose=true, seconds=20)
